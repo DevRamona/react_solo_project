41926683d8ac09e9bcf1aba108147be2c724ce47
DevRamona <r.ingabire@alustudent.com>
2025-04-06 22:59:13 +0200

    add some few changes on the about section

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Add the about us section</div>
+    <div>Add the about us section
+        <h1>About Us</h1>
+        <p>We are a team of passionate developers dedicated to creating amazing web applications.</p>
+    </div>
   )
 }
 
