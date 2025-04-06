d4f53e779b5048504965e65724bbbdeea51c71df
DevRamona <r.ingabire@alustudent.com>
2025-04-06 22:58:42 +0200

    add some few changes

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react'
 
-export default function About() {
-    return (
-        <div className="text-white pl-7 pt-7">
-            <h1 className="text-2xl text-center pr-44">About</h1>
-            <p className="w-1/5 text-sm mx-auto text-left">I am a frontend developer with a particular interest in making things simple
-                 and automating daily tasks. I try to keep up with security and best practices,
-                  and am always looking for new things to learn.</p>
-        </div>
-    )
+const About = () => {
+  return (
+    <div>Add the about us section</div>
+  )
 }
+
+export default About
