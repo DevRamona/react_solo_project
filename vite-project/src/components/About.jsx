011f3778cd134534c6eb86d093c5f67922769a63
DevRamona <r.ingabire@alustudent.com>
2025-06-09 21:56:23 +0200

    add content

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
     <div>Add the about us section
         <h1>About Us</h1>
         <p>We are a team of passionate developers dedicated to creating amazing web applications.</p>
+        <p>We are a team of passionate developers dedicated to creating amazing web applications.</p>
     </div>
   )
 }
