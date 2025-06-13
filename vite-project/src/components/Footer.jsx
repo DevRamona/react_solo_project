1e8786a55f21d9648030b1386f9b5414e7d7babc
Ramona Ingabire <139565706+DevRamona@users.noreply.github.com>
2025-06-13 22:37:41 +0200

    Update Footer.jsx

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -10,6 +10,7 @@ export default function Footer() {
         <div className="flex pt-5 pr-16 space-x-3">
             
             <a href="#"><img src={facebook} className="bg-gray-200"></img></a>
+            <a href="#"><img src={github}className="bg-gray-200"></img></a>
             <a href="#"><img src={linkedin} className="bg-gray-200"></img></a>
             <a href="#"><img src={twitter} className="bg-gray-200"></img></a>
             <a href="#"><img src={instagram}className="bg-gray-200"></img></a>
